d498845179421737e68772362e3cbc2e410bcf0c
hongphucvo <vohongphuc2001@gmail.com>
2021-09-09 15:01:15 +0700

    week3_part1

--- programming code/OOP_week3.py
+++ programming code/OOP_week3.py
@@ -1,49 +1,53 @@
 class Exp:
-    def eval():
-    def printPrefix():
+    def eval():pass
+    def printPrefix():pass
 
 class BinExp(Exp):
-    operand1=0
-    operator="+"
-    operand2=0
     def __init__(self, operand1, operator, operand2):
         self.operand1=operand1
         self.operator=operator
         self.operand2=operand2
-    def val(self):
+    def eval(self):
         if(self.operator=="+"):
-            return self.operand1 + self.operand2
+            return self.operand1.eval() + self.operand2.eval()
         elif(self.operator=="-"):
-            return self.operand1-self.operand2
+            return self.operand1.eval()-self.operand2.eval()
         elif(self.operator=="/"):
-            return self.operand1/self.operand2
+            return self.operand1.eval()/self.operand2.eval()
         else:
-            return self.operand1*self.operand2
+            return self.operand1.eval()*self.operand2.eval()
     def printPrefix():
-        return operand1+" "+operator+" "+operand2
+        operand1.printPrefix()+" "+operator+" "+operand2.printPrefix()
 class UnExp(Exp):
-    operator="+"
-    operand=0
     def __init__(self, operator, operand):
         self.operator=operator
         self.operand=operand
-    def val(self):
+    def eval(self):
         if(self.operator=="+"):
-            return self.operand
-        else: return -self.operand
-
+            return self.operand.eval()
+        else: return -self.operand.eval()
+    def printPrefix():
+        operand1.printPrefix()+" "+operator+". "+operand2.printPrefix()
 class IntLit(Exp):
-    number=0
     def __init__(self, value):
         self.number=value   
-    def val(self):
+    def eval(self):
         return int(self.number)
-
+    def printPrefix(self):
+        print(self.number)
 class FloatLit(Exp):
-    number=0.0
     def __init__(self,value):
         self.number=value
-    def val(self):
+    def eval(self):
         return float(self.number)
+    def printPrefix(self):
+        print(self.number)
+x1 = IntLit(1)
+x2 = FloatLit(2.0)
+x3 = BinExp(x1,"+",x1)
+x3.printPrefix()
+x4 = UnExp("-",x1)
 
-x = Exp(3 + 4 * 2.0);
+print(x4.eval())
+x5 = BinExp(x4,"+",BinExp(IntLit(4),"*",x2))
+print(x5.eval())

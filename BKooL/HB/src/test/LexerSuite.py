51392553847d3827b9ebb672ed80e76ba907c439
hongphucvo <vohongphuc2001@gmail.com>
2022-01-04 22:41:00 +0700

    clean dump

--- BKooL/HB/src/test/LexerSuite.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from TestUtils import TestLexer
-
-class LexerSuite(unittest.TestCase):
-      
-    def test_lowercase_identifier(self):
-        pass
--- BKooL/HB/src/test/ParserSuite.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from TestUtils import TestParser
-
-class ParserSuite(unittest.TestCase):
-    def test_simple_program(self):
-        input = """class A{int x(){ int m;for a:=4.5 to 10 do {int x;}}}"""
-        expect = "successful"
-        self.assertTrue(TestParser.test(input, expect, 202))
